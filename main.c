d5ecadb1a30f0e28efc41e26ba6de31d9570da0b
Len HIRATA <len@hm00m26.local>
2020-02-24 10:50:20 +0900

    bug

--- main.c
+++ main.c
@@ -14,11 +14,12 @@ long unsigned int c2b(int x, int y) {
     return bitset;
 }
 
-void printb(long unsigned int bitset) {
+void printb(long unsigned int black, long unsigned int white) {
     for (int y = 0; y < HEIGHT; y++) {
         for (int x = 0; x < WIDTH; x++) {
-            if (bitset >> (x + y * WIDTH) & 1) putchar('1');
-            else putchar('0');
+            if (black >> (x + y * WIDTH) & 1) putchar('#');
+            else if (white >> (x + y * WIDTH) & 1) putchar('.');
+            else putchar(' ');
             putchar(' ');
         }
         putchar('\n');
@@ -28,10 +29,19 @@ void printb(long unsigned int bitset) {
 
 int main() {
     long unsigned int black = c2b(3, 3) | c2b(4, 4), white = c2b(3, 4) | c2b(4, 3);
+    int turn = 0;
 
-    while (1) {
+    while (turn++) {
         int x, y;
-        scanf("x<< %d", &x);
-        scanf("y<< %d", &x);
+        printf("x<< ");
+        scanf("%d", &x);
+        printf("y<< ");
+        scanf("%d", &y);
+
+        long unsigned int hand = c2b(x, y);
+        if (turn % 2) black |= hand;
+        else white |= hand;
+
+        printb(black, white);
     }
 }
